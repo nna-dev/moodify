f615c2a41a58edf5e08943b9a67a08b0e811668c
nawo <nna.dev.950910@gmail.com>
2023-09-11 23:37:09 +0700

    process chain instead of throw exception

--- app/src/main/java/com/nna/moodify/data/client/AuthorizationHeaderInterceptor.kt
+++ app/src/main/java/com/nna/moodify/data/client/AuthorizationHeaderInterceptor.kt
@@ -3,20 +3,24 @@ package com.nna.moodify.data.client
 import com.nna.moodify.domain.repositories.AuthRepository
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 import javax.inject.Inject
 
 class AuthorizationHeaderInterceptor @Inject constructor(
     private val authRepository: AuthRepository
 ): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        try {
+        return try {
             val token = authRepository.getValidToken()
             val newRequest = chain.request().newBuilder()
                 .addHeader("Authorization", token.value)
                 .build()
-            return chain.proceed(newRequest)
+            chain.proceed(newRequest)
         } catch (e: Exception) {
-            throw e
+            Timber.e(e)
+
+            // TODO: process or return unauthorized
+            chain.proceed(chain.request())
         }
     }
 }
