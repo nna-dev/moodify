d8ca439a161e193eb51b8dd18a27e66aaa1ce8bc
nawo <nna.dev.950910@gmail.com>
2023-09-29 23:26:20 +0700

    Update some

--- app/src/main/java/com/nna/moodify/ui/browse/BrowseViewModel.kt
+++ app/src/main/java/com/nna/moodify/ui/browse/BrowseViewModel.kt
@@ -15,7 +15,7 @@ class BrowseViewModel @Inject constructor(
     private val getCategoriesUseCase: GetCategoriesUseCase
 ) : ViewModel() {
     private val _categories = flow {
-        emit(getCategoriesUseCase(8).successOr(emptyList()))
+        emit(getCategoriesUseCase(50).successOr(emptyList()))
     }
 
     val categories = _categories.shareIn(viewModelScope, SharingStarted.WhileSubscribed(), 1)
